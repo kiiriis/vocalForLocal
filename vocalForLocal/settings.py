a373e40fbf55266d8e7fd124acf835f0005d99a7
Murtaza M <murtazamister1@gmail.com>
2022-01-10 23:45:52 +0530

    Project Initialized

--- vocalForLocal/settings.py
+++ vocalForLocal/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&k3d7s^k*ekr6&&4@*3do1dmb4%f9fd@np9@^zw5zq3b4lk*hu'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'vocalForLocal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vocalForLocal',
+        'USER' : 'postgres',
+        'PASSWORD' : os.getenv('DB-PASSWORD'),
+        'HOST' : 'localhost',
     }
 }
 
