4ab5d39834c9785097ab14f072f14448977ada26
Murtaza M <murtazamister1@gmail.com>
2022-01-26 12:34:08 +0530

    FrontEnd

--- static/base/base.js
+++ static/base/base.js
@@ -62,6 +62,32 @@ checkbox.addEventListener('change', ()=>{
     $('#body').toggleClass('dark');
 })
 
-$(document).ready(function(){       
+// Ripple
+
+var addRippleEffect = function (e) {
+    var target = e.target;
+    if (target.tagName.toLowerCase() !== 'button') return false;
+    var rect = target.getBoundingClientRect();
+    var ripple = target.querySelector('.ripple');
+    if (!ripple) {
+        ripple = document.createElement('span');
+        ripple.className = 'ripple';
+        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+        target.appendChild(ripple);
+    }
+    ripple.classList.remove('show');
+    var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    ripple.style.top = top + 'px';
+    ripple.style.left = left + 'px';
+    ripple.classList.add('show');
+    return false;
+}
+
+$(document).ready(function(){
     navShadowHandler();
-});
+    let buts = document.getElementsByClassName("rippler");
+    for (let i = 0; i < buts.length; i++) {
+        buts[i].addEventListener('click', addRippleEffect, false);
+    }
+})
