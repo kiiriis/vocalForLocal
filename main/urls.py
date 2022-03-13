cd1ed7738b04f95c7b8eb57607afff29f95ebfda
Krish Makadia <makadiakrish@gmail.com>
2022-03-13 14:07:16 +0530

    business signup

--- main/urls.py
+++ main/urls.py
@@ -5,16 +5,17 @@ from main import views
 from main import utility
 
 urlpatterns = [
-    path('',views.index,name="index"),
-    path('community',views.community,name="community"),
-    path('login',views.loginUser,name="login"),
-    path('logout',views.logoutUser,name="logout"),
-    path('signup',views.signUp,name="signUp"),
-    path('dashboard',views.dashboard,name="dashboard"),
-    path('editProfile',views.editProfile,name="editProfile"),
-    path('resetPassword',views.resetPassword,name="resetPassword"),
-    path('sendEmailOtp',utility.sendEmailOtp,name="sendEmailOtp"),
-    path('unameChecker',utility.unameChecker,name="unameChecker"),
-    path('emailChecker',utility.emailChecker,name="emailChecker"),
-    path('phoneChecker',utility.phoneChecker,name="phoneChecker"),
+    path('', views.index, name="index"),
+    path('community', views.community, name="community"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+    path('signup', views.signUp, name="signUp"),
+    path('dashboard', views.dashboard, name="dashboard"),
+    path('editProfile', views.editProfile, name="editProfile"),
+    path('resetPassword', views.resetPassword, name="resetPassword"),
+    path('sendEmailOtp', utility.sendEmailOtp, name="sendEmailOtp"),
+    path('unameChecker', utility.unameChecker, name="unameChecker"),
+    path('emailChecker', utility.emailChecker, name="emailChecker"),
+    path('phoneChecker', utility.phoneChecker, name="phoneChecker"),
+    path('businessSingup', views.businessSignup, name="businessSignup")
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
+from unicodedata import name
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from main.models import User
 from dotenv import load_dotenv
-from main.helper import getTheme,redirector,isAnonymous
+from main.helper import getTheme, redirector, isAnonymous
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
 import os
@@ -11,13 +12,17 @@ import os
 load_dotenv()
 
 # Create your views here.
+
+
 def index(request):
     theme = getTheme(request)
-    return render(request,'index.html',{'theme':theme})
+    return render(request, 'index.html', {'theme': theme})
+
 
 def community(request):
     theme = getTheme(request)
-    return render(request,'community.html',{'theme':theme,'token':os.getenv('MAP_ACCESS_TOKEN')})
+    return render(request, 'community.html', {'theme': theme, 'token': os.getenv('MAP_ACCESS_TOKEN')})
+
 
 @user_passes_test(isAnonymous, login_url='/')
 def loginUser(request):
@@ -26,23 +31,24 @@ def loginUser(request):
         username = ''
         if request.GET.get('username'):
             username = request.GET.get('username')
-        return render(request,'login.html',{'theme':theme,'username':username})
+        return render(request, 'login.html', {'theme': theme, 'username': username})
     else:
         theme = request.POST['theme']
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
-            login(request,user)
-            return redirect(redirector('/dashboard',theme))
+            login(request, user)
+            return redirect(redirector('/dashboard', theme))
         else:
             messages.error(request, "INVALID CREDENTIALS!")
-            return redirect(redirector('/login',theme,f'username={username}'))
+            return redirect(redirector('/login', theme, f'username={username}'))
+
 
 def resetPassword(request):
     if request.method != "POST":
         theme = getTheme(request)
-        return render(request,'resetPassword.html',{'theme':theme})
+        return render(request, 'resetPassword.html', {'theme': theme})
     else:
         theme = request.POST['theme']
         email = request.POST['email']
@@ -51,21 +57,23 @@ def resetPassword(request):
         u.set_password(password)
         u.save()
         messages.info(request, "Your password has been reset")
-        return redirect(redirector('/login',theme,f'username={u.username}'))
+        return redirect(redirector('/login', theme, f'username={u.username}'))
+
 
 @login_required(login_url='/login')
 def logoutUser(request):
     theme = getTheme(request)
     if not request.user.is_anonymous:
         logout(request)
-    return redirect(redirector('/',theme))
+    return redirect(redirector('/', theme))
+
 
 @user_passes_test(isAnonymous, login_url='/')
 def signUp(request):
     if request.method != "POST":
         theme = getTheme(request)
-        return render(request, "signup.html", {'theme':theme,'csc_email':os.getenv('CSC_EMAIL'),'csc_token':os.getenv('CSC_API_TOKEN')})
-    else :
+        return render(request, "signup.html", {'theme': theme, 'csc_email': os.getenv('CSC_EMAIL'), 'csc_token': os.getenv('CSC_API_TOKEN')})
+    else:
         theme = request.POST['theme']
         if request.FILES.get('avatar') == None:
             u = User(
@@ -86,7 +94,7 @@ def signUp(request):
             )
         else:
             u = User(
-                display_pic = request.FILES.get('avatar'),
+                display_pic=request.FILES.get('avatar'),
                 first_name=request.POST['first_name'],
                 last_name=request.POST['last_name'],
                 username=request.POST['username'],
@@ -105,13 +113,24 @@ def signUp(request):
         u.set_password(request.POST['password'])
         u.save()
         messages.success(request, 'Welcome to #vocalForLocal')
-        return redirect(redirector('/login',theme,f'username={request.POST["username"]}'))
+        return redirect(redirector('/login', theme, f'username={request.POST["username"]}'))
+
 
 @login_required(login_url='/login')
 def dashboard(request):
     theme = getTheme(request)
-    return render(request,'dashboard.html',{'theme':theme})
+    return render(request, 'dashboard.html', {'theme': theme})
+
 
 @login_required(login_url='/login')
 def editProfile(request):
     return HttpResponse("Coming soon...")
+
+
+def businessSignup(request):
+    if(request.method == "GET"):
+        theme = getTheme(request)
+        return render(request, 'businessSignup.html', {'theme': theme})
+    else:
+        print("hello")
+        return HttpResponse("You are signed")
